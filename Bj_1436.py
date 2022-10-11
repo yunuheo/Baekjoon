a66471632dc4b5f485ecf5c782449202f6dec220
허윤우 <hyoon516@naver.com>
2022-10-12 03:50:32 +0900

    순차탐색 알고리즘 사용
    666부터 1씩 증가시키면서 문자열 '666'을 포함하는 모든 수를 변수cnt에 담고, cnt가 N과 같아졌을 때 여태까지 더해온 수를 출력하도록 함

--- Bj_1436.py
+++ Bj_1436.py
@@ -1,1 +1,16 @@
+
 N = int(input())
+
+count_six = 666    #666이 기본값 => 1씩 더해나가면서 검사해줌
+cnt = 0            #666이 들어있는 수들이 출현하는 횟수
+while True:
+    if '666' in str(count_six):  # (*중요*) 666을 문자열로 취급하고 증가하는 숫자의 문자열에 666이 있으면 카운팅.
+        cnt += 1
+    if cnt == N:
+        break
+    count_six += 1   #조건에 해당하지 않으면 계속해서 1씩 더해나감(순차탐색 알고리즘)
+
+print(count_six)     #cnt가 N과 같아졌을때 여태까지 최종결과 count_six 출력
+
+    
+     
