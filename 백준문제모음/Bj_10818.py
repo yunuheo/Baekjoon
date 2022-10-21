38674744bd1ac77a3904b9d34b443239f09231db
허윤우 <hyoon516@naver.com>
2022-10-22 04:41:12 +0900

    max와 min함수 사용
    -파이썬에서 제공하는 함수를 사용하여 해결할 수도 있음(max(),min())

--- 백준문제모음/Bj_10818.py
+++ 백준문제모음/Bj_10818.py
@@ -2,19 +2,6 @@
 
 N = int(input())
 arr = list(map(int,input().split(' ')))
-'''
-min = arr[0]
-max = arr[0]
-for i in range(N):
-    if min > arr[i]:
-        min = arr[i]
-
-for i in range(N):
-    if max < arr[i]:
-        max = arr[i]
-
-print(min,max)
-'''
 
 max = max(arr)
 min = min(arr)
