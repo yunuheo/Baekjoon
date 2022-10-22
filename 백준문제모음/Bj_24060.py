707f840cdde4f38fbc18b728093d6624df7d2247
허윤우 <hyoon516@naver.com>
2022-10-23 04:17:19 +0900

    배열A에 k번째로 저장된 수
    -merge함수 내부에서 배열A에 값이 들어올때마다 cnt에 1씩 더해줌
    -위 과정에서 내가 입력한 k번째 저장 된 수를 구하기 위해 cnt=k 가 될 때 배열A에 저장된 수를 전역변수 result에 넣기

--- 백준문제모음/Bj_24060.py
+++ 백준문제모음/Bj_24060.py
@@ -1,15 +1,22 @@
 #병합정렬을 이용하여 정렬된 배열tmp[]에 k번째로 저장된 숫자 찾기.
 
 #병합정렬 구현
+
+N,k = map(int,input().split(' '))
+A = list(map(int,input().split(' ')))
+
+
 def merge_sort(A,p,r):
     if p<r:
         q = (p+r)//2
         merge_sort(A,p,q)
         merge_sort(A,q+1,r)
         merge(A,p,q,r)
-        return A
         
+       
 def merge(A,p,q,r):
+    global cnt  #함수 안에서 전역변수 cnt를 사용하기 위해 global 키워드를 사용!!!!!
+    global result
     i = p
     j = q+1
     tmp = []
@@ -30,14 +37,23 @@ def merge(A,p,q,r):
     
     i = p
     t = 0
+    
+      
+    
+    
     while i<=r:
         A[i] =tmp[t]
+        cnt += 1  #정렬된 수가 배열A에 저장될때마다 1씩 증가
+        if cnt == k:
+            result = A[i]
+            break
         i += 1
         t += 1
+    
 
-arr1 = [6,13,1,49]
-
-print(merge_sort(arr1,0,3))
-
+cnt = 0  #cnt는 전역변수임 바로 아래 merge_sort 함수 내에서 사용될 예정(=>global 키워드 사용)
+result = 0
+merge_sort(A,0,N-1)  
+print(result)
 
 
