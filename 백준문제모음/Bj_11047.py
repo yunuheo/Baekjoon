4b0615600d88c3ceb07fce21b8d0503e066abcee
허윤우 <hyoon516@naver.com>
2022-11-01 08:43:40 +0900

    나머지연산 사용
    -이전 코드는 시간초과를 발생시킴 => 나머지연산 사용하여 속도를 향상시킴

--- 백준문제모음/Bj_11047.py
+++ 백준문제모음/Bj_11047.py
@@ -5,17 +5,13 @@ for _ in range(N):
     arr.append(int(input()))
 
 cnt = 0
-while True:
-  for _ in range(N):
-    if max(arr)>k:
-        arr.remove(max(arr))
-  if k == max(arr):
-    cnt += 1
-    break
-  k -= max(arr)
-  cnt += 1
-    
-
-
+for i in range(N - 1, -1, -1): #동전의 가치가 오름차순으로 주어졌으므로 range함수를 사용하여 후위연산
+    if k == 0:
+        break
+    if arr[i] > k:
+        continue    
+    cnt += k//arr[i]
+    k %= arr[i]
+ 
 print(cnt)
 
