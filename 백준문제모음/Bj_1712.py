c6e3329a9e175eac8f5281ca114cb6c5b06beb25
허윤우 <hyoon516@naver.com>
2022-10-31 19:39:37 +0900

    반복문 빼고 수식 사용
    -시간초과로 인해 while문을 없애고 판매량 n을 기준으로 수식을 만들어 해당값에 1을 더한 값이 손익분기점으로 파악함

--- 백준문제모음/Bj_1712.py
+++ 백준문제모음/Bj_1712.py
@@ -5,15 +5,22 @@ A,B,C = map(int,input().split(' '))
 #C = 책정된 노트북 가격
 # 손익분기점 = C의 가격
 
-#1000+70*n < 170*n ==> A+B*n < C*n 일때의 n을 구하면 될 듯
+#1000+70*n < 170*n ==> A+B*n < C*n 일때의 n을 구하면 될 듯 (n은 판매량)
 
-n = 1
+'''n = 1
 while True:
     if C == 1 or B >= C:
         n = -1
         break
-    if A+B*n < C*n:
+    elif A//(C-B) < n:
         break
     n += 1
 
 print(n)
+'''
+
+if C == 1 or B >= C:
+    print(-1)
+else:
+    print(A//(C-B)+1)   #판매량 n을 기준으로 식을 이항하여 표현(나누기 연산시 소수점을 피하기 위해 // 연산식 사용)
+                        #판매량이 A//(C-B)를 넘어야 하기 때문에 1을 더해줘야함
