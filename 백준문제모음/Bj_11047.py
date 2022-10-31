1de3bd1b27fc9a82ef041acb60e5d7bde7caed80
허윤우 <hyoon516@naver.com>
2022-11-01 07:43:30 +0900

    시간초과
    -해당 로직으로 예제 1,2는 해결되지만 백준 채점시 시간초과가 뜸

--- 백준문제모음/Bj_11047.py
+++ 백준문제모음/Bj_11047.py
@@ -3,3 +3,19 @@ N,k = map(int,input().split(' '))
 arr = []
 for _ in range(N):
     arr.append(int(input()))
+
+cnt = 0
+while True:
+  for _ in range(N):
+    if max(arr)>k:
+        arr.remove(max(arr))
+  if k == max(arr):
+    cnt += 1
+    break
+  k -= max(arr)
+  cnt += 1
+    
+
+
+print(cnt)
+
