4da3396f0a8be1bdf63d35b049aacb89f4163730
허윤우 <hyoon516@naver.com>
2022-10-31 19:21:02 +0900

    버그발생
    -입력값이 작으면 풀리지만 A가 21억일때 버그 발생

--- 백준문제모음/Bj_1712.py
+++ 백준문제모음/Bj_1712.py
@@ -7,3 +7,13 @@ A,B,C = map(int,input().split(' '))
 
 #1000+70*n < 170*n ==> A+B*n < C*n 일때의 n을 구하면 될 듯
 
+n = 1
+while True:
+    if C == 1 or B >= C:
+        n = -1
+        break
+    if A+B*n < C*n:
+        break
+    n += 1
+
+print(n)
