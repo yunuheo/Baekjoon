7d5db00fbddfb0486db42db354dc313455518d80
허윤우 <hyoon516@naver.com>
2022-11-06 20:04:26 +0900

    이중for문 및 pass문 사용
    -N번 반복될동안 word에 문자를 입력받음
    -word변수에 담긴 문자를 반복문으로 검사함
    -다음글자와 같으면 pass
    -같은 문자 사이에 다른 문자가 오는 경우 result 값이 1 감소
    -검사가 끝난 후 result 출력

--- 백준문제모음/Bj_1316.py
+++ 백준문제모음/Bj_1316.py
@@ -1,6 +1,19 @@
 #그룹단어는  문자가 단독으로 나오거나 연속해서 나와야함.
 #따라서 같은 문자 사이에는 다른 문자가 와서는 안됨.
-list = []
+
 N = int(input())
+result = N
 for _ in range(N):
-    list.append(input())
+    word = input()
+    for j in range(0,len(word)-1):
+       if word[j] == word[j+1]:
+        pass  #pass => 조건이 참이면 아무것도 실행안함
+       elif word[j] in word[j+1:]:
+        result -= 1
+        break
+
+print(result)
+
+
+
+
