f6615100161c2a4c09b0eeb441988485750360ac
허윤우 <hyoon516@naver.com>
2022-11-27 19:21:58 +0900

    높이를 기준으로 수식을 만듦, 여기서 정상에 올랐을때는 미끄러지지않으므로 1일을 따로 더해줌

--- 백준문제모음/Bj_2869.py
+++ 백준문제모음/Bj_2869.py
@@ -1,16 +1,6 @@
+import math
+
 A, B, V = map(int,(input().split(' ')))
 
-Up_perDay = 0
-day = 0
-
-while True:
-   
-    Up_perDay +=  A-B
-    
-    if Up_perDay == V:
-        break
-    elif Up_perDay > V:
-        break
-    day += 1
-
+day = math.ceil((V-A)/(A-B)+1)
 print(day)
