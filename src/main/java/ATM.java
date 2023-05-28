0f8a8cb8559ae3bb46c3732c95db3080657d7c47
허윤우 <hyoon516@naver.com>
2023-05-28 22:48:51 +0900

    client 배열에 각 인원의 대기시간을 담고 오름차순 정렬을 수행한 뒤 각 인원의 대기시간을 계산하여 더해주었다.

---
+++ src/main/java/ATM.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class ATM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int[] cli = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            cli[i] = sc.nextInt();
+        }
+
+        Arrays.sort(cli); //cli 배열 오름차순 정렬
+        int sum = 0;
+
+
+        for (int i = 1; i <= cli.length; i++) {
+            for (int j = 0; j < i; j++) {
+                sum = sum + cli[j];
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
