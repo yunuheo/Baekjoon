10dfbd21a6c9330d6999b93bf840932dbef8fff5
허윤우 <hyoon516@naver.com>
2023-05-28 23:13:01 +0900

    client 배열에 각 인원의 대기시간을 담고 오름차순 정렬을 수행한 뒤 각 인원의 대기시간을 계산하여 더해주었다.
    (문제번호 및 링크 첨부)

--- 백준문제모음/ATM.java
+++ 백준문제모음/ATM.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    // 백준 11399번 (링크:https://www.acmicpc.net/problem/11399)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
